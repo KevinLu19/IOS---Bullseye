03ab3bfa0af45b33ea4bc3ae11427fc05b873131
Kevin <rumi@Rumis-Mac.local>
2020-02-10 23:14:26 -0500

    Added alert for current slider data back to user

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -8,23 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController
+{
+    // Default value of the slider. Trying to remember the data on the sliders.
+    var currentValue: Int = 50
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    // For the button.
     @IBAction func showAlert()
     {
-        let alert = UIAlertController(title: "Hello, world", message: "This is my first app!", preferredStyle: .alert)
+        let message = "The value of the slider is: \(currentValue)"
         
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
-        
+        let alert = UIAlertController(title: "Hello, World",
+                                      message: message,
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok",
+                                   style: .default,
+                                   handler:nil)
         alert.addAction(action)
-        
         present(alert, animated: true, completion: nil)
+    }
+    
+    // For the slider. Reading data on the slider.
+    @IBAction func sliderMoved(_ slider: UISlider)
+    {
+        currentValue = lroundf(slider.value)
     }
 }
 
